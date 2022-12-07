624cab56e8895cd522d83af17a4176aa201aecaa
hasan9948 <118598080+hasan9948@users.noreply.github.com>
2022-12-07 23:46:15 +0530

    new update to todo list

--- todolist/js/todolist.js
+++ todolist/js/todolist.js
@@ -2,6 +2,11 @@ const body =document.body;
 const ul=document.querySelector(".todo-ul")
 const form=document.querySelector(".todo-form")
  const input=document.querySelector(".todo-form input[type=\"text\"]")
+ const msg=document.querySelector(".msg")
+ console.log(msg)
+ const remove_msg=document.querySelector(".remove-msg")
+
+
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -14,6 +19,11 @@ li.innerHTML=newli
 console.log(li)
 ul.prepend(li);
 li.classList.add('todo-li')
+setInterval(() => {
+  msg.style.display="none"
+}, 3000);
+
+msg.style.display="block"
 input.value=""
 })
 
@@ -26,5 +36,12 @@ ul.addEventListener("click",(e)=>{
   if(e.target.classList.contains("remove")){
     const r= e.target.parentNode.parentNode;
     r.remove();
+    remove_msg.style.display="block"
+setInterval(() => {
+  remove_msg.style.display="none"
+}, 3000);
+
   }
 })
+
+
